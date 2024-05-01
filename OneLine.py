439218d4b0a26ebbac9e8f17dfd540cb790700bc
Matheus <125626334+MatheusDev9339@users.noreply.github.com>
2024-05-01 16:21:03 -0300

    Update OneLine.py

--- OneLine.py
+++ OneLine.py
@@ -1,1 +1,1 @@
-A,B,C=int(input('A: ')),int(input('B: ')),int(input('C: '));print(f"X¹ = {((-B)+(((B**2)-(4*A*C))**0.5))/(2*A)}\nX² = {((-B)-(((B**2)-(4*A*C))**0.5))/(2*A)}")
+while True:A,B,C=int(input('A: ')),int(input('B: ')),int(input('C: '));print(f"X¹ = {((-B)+(((B**2)-(4*A*C))**0.5))/(2*A)}\nX² = {((-B)-(((B**2)-(4*A*C))**0.5))/(2*A)}")
